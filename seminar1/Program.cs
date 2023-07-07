da04c002519064e1780438b0c9f54095fb107a0d
«ИвановСтанислав» <stas_ivanov53@mail.ru>
2023-07-07 15:41:20 +0300

    add seminar

---
+++ seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿// int num1 = 1;
+// int num2 = 10;
+
+// Console.WriteLine("Numbers: " + num1 + " and " + num2 );
+// Console.WriteLine($"Numbers: {num1} and {num2}");
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Your number is " + num);
