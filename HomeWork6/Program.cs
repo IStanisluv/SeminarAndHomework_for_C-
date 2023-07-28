2070a049286e77c99517ee3ce28698d47a1bed62
«ИвановСтанислав» <stas_ivanov53@mail.ru>
2023-07-28 20:17:50 +0300

    Add Homework6

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,42 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+int[] CreateArray (int size)
+{
+    int[] array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-20, 20);
+
+}
+    return array;
+}
+
+
+void ShowArray(int [] array)
+{
+    for ( int i = 0; i < array.Length; i++)
+        Console.Write(array[i]+" ");
+    Console.WriteLine();
+}
+
+int NumPozitive( int []array)
+{
+    int count =0;
+    for(int i = 0; i < array.Length;i++)
+    {
+        if(array[i]>0) count++;
+    }
+    return count;
+}
+
+Console.Write("Input a size:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = CreateArray(size);
+ShowArray(myArray);
+int pozitiveNum = NumPozitive(myArray);
+Console.WriteLine("Pozitive number in array is " +pozitiveNum);
+*/
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
